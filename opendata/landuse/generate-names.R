234fd84911ba2fa2fae5db13403f98387edc4701
Hans Thompson <hans.thompson1@gmail.com>
2016-03-07 10:52:30 -0900

    structured folders

---
+++ opendata/landuse/generate-names.R
@@ -0,0 +1,11 @@
+html <- list.files("/home/ht/Desktop/git/hansthompson.github.io/opendata/landuse/html")
+
+links <- gsub(".html", "", html)
+
+href <- paste0("<h2>* <a href='http://akdata.org/opendata/landuse/", html,  "'>",
+               links, "</a></h2>")
+
+
+fileConn<-file("/home/ht/Desktop/git/hansthompson.github.io/opendata/landuse/community-councils.html")
+writeLines(href, fileConn)
+close(fileConn)
