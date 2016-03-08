e61b4fd3eee8d25a92ba4fd415647ac0f3dfd20c
Hans Thompson <hans.thompson1@gmail.com>
2016-03-07 16:25:51 -0900

    added add the landuse

--- opendata/landuse/generate-names.R
+++ opendata/landuse/generate-names.R
@@ -1,8 +1,8 @@
-html <- list.files("/home/ht/Desktop/git/hansthompson.github.io/opendata/landuse/html")
+html <- list.files("/home/ht/Desktop/git/hansthompson.github.io/opendata/landuse/cc")
 
 links <- gsub(".html", "", html)
 
-href <- paste0("<h2> <a href='http://akdata.org/opendata/landuse/html/", gsub(" ", "%20", html),  "'>",
+href <- paste0("<h2> <a href='http://akdata.org/opendata/landuse/cc/", gsub(" ", "%20", html),  "'>",
                links, "</a></h2>")
 
 
